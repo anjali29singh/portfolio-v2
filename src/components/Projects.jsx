c954604850049820e9db89d12288b60c662481ca
anjali29singh <anjalisingh930260@gmail.com>
2023-06-21 22:48:47 +0530

    completed portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
 
   return (
     <div name="projects" className="w-full md:h-screen text-white bg-[#020f15]">
-      <div className="max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full s">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-white border-pink-600">
             Projects
@@ -21,10 +21,15 @@ const Projects = () => {
           {project.map((item, index) => (
             <div
               key={index}
-              className=" group container rounded-md 
-              flex  flex-col justify-center text-center items-start mx-auto  gap-4 "
+              className=" group container rounded-md pl-10
+              flex  flex-col justify-center text-center items-start mx-auto  gap-4 shadow-xl ring-2 ring-slate-700"
             >
-              <img src={item.image} alt="shrinkyImg" className="w-4/5"></img>
+              <img
+                src={item.image}
+                alt="shrinkyImg"
+                className="w-2/3 ring-1 ring-gray-800"
+              ></img>
+
               <p className="text-cyan-300 ">
                 {item.name} {item.intro}
               </p>
@@ -34,6 +39,9 @@ const Projects = () => {
             </div>
           ))}
         </div>
+        <h2 className="text-slate-300 m-3">
+          email : anjalisinghdev2@gmail.com
+        </h2>
       </div>
     </div>
   );
